fd93b98fea1617c99f7fcea522827bd51aee59a1
unknown <johnebabinjr@gmail.com>
2019-10-26 10:45:08 -0700

    create react app and images for project

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import tauren from './images/tauren.jpg';
+import orc from './images/orc.jpg';
+import troll from './images/troll.jpg';
+import undead from './images/undead.jpg';
+import goblin from './images/goblin.jpg';
+import human from './images/human.jpg';
+import dwarf from './images/dwarf.jpg';
+import gnome from './images/gnome.jpg';
+import nightelf from './images/nightelf.jpg';
+import worgen from './images/worgen.jpg';
+import draenei from './images/draenei.jpg';
+import bloodelf from './images/bloodelf.jpg';
+
+const images = [
+  tauren,
+  orc,
+  troll,
+  undead,
+  goblin,
+  human,
+  dwarf,
+  gnome,
+  nightelf,
+  worgen,
+  draenei,
+  bloodelf
+];
+
+export default images;
